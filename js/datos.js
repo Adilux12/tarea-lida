a12d3e71403af281bd258d3e08428fb8d3e3a734
LidaXu <xulida12@gmail.com>
2023-02-26 23:40:18 +0100

    pintar lista de tareas.

---
+++ js/datos.js
@@ -0,0 +1,28 @@
+let idTarea = 6;
+const listaTareas = new Array(
+    {
+        idTarea: 1,
+        titulo: 'Estudiar Javascript',
+        prioridad: 'urgente'
+    },
+    {
+        idTarea: 2,
+        titulo: 'Dormir',
+        prioridad: 'diaria'
+    },
+    {
+        idTarea: 3,
+        titulo: 'Salir a comer',
+        prioridad: 'mensual'
+    },
+    {
+        idTarea: 4,
+        titulo: 'Terminar proyecto',
+        prioridad: 'urgente'
+    },
+    {
+        idTarea: 5,
+        titulo: 'Hacer ejercicio',
+        prioridad: 'diario'
+    },
+)
---
+++ js/view.js
@@ -0,0 +1,41 @@
+//captura pantalla de la secctiom pintar tarea
+const sectionTask = document.querySelector('section .flex');
+console.log(sectionTask);
+
+
+function printAllTasks(pListTask, pSectionTask) {
+    pListTask.forEach(task => printOneTask(task, sectionTask));
+}
+
+function printOneTask(pTask, pSectionTask) {
+    /**       < article >
+                        <h3>Titulo de tarea </h3>
+                        <div>
+                            <button>x</button>
+                        </div>
+                    </article >
+        <article>
+            <h3>Titulo de tarea </h3>
+            <div>
+                <button>x</button>
+            </div>
+        </article>-*/
+
+    const article = document.createElement('article');
+    const h3 = document.createElement('h3');
+    h3.textContent = pTask.titulo;
+    const div = document.createElement('div');
+    const button = document.createElement('button');
+    button.textContent = 'x';
+    article.classList.add('urgente')
+
+
+    article.appendChild(h3);
+    article.appendChild(div);
+    div.appendChild(button);
+    pSectionTask.appendChild(article);
+
+}
+
+
+printAllTasks(listaTareas, sectionTask);
